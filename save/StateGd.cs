2d56878b0f7ae7098465a60ee0d4e4db349282f0
Artem Getmanskii <re910@yandex.ru>
2026-04-20 14:53:38 +0300

    resave balance

--- save/StateGd.cs
+++ save/StateGd.cs
@@ -1,32 +1,25 @@
 using Godot;
 using System;
+using System.IO;
 using TreasureHunter.balance;
 using TreasureHunter.save;
+using FileAccess = Godot.FileAccess;
 
 public partial class StateGd : Node2D
 {
-    public IState state;
-    public IBalance balance;
+	public IState state;
+	public IBalance balance;
 
-    public override void _Ready()
-    {
-        var path = "res://resources/LD59.csv";
+	public override void _Ready()
+	{
+		var text = FileAccess.GetFileAsString("res://resources/LD59.csv");
+		File.WriteAllText(AppDomain.CurrentDomain.BaseDirectory + "/balance.csv", text);
+		this.balance = new Balance(AppDomain.CurrentDomain.BaseDirectory + "/balance.csv");
+		this.state = new StateCsv(AppDomain.CurrentDomain.BaseDirectory + "/saves");
+	}
 
-        GD.Print(FileAccess.FileExists(path));
-
-        GD.Print(ResourceLoader.Exists(path));
-
-        var text = FileAccess.GetFileAsString(path);
-        GD.Print(text);
-        // GD.Print(balancePath.ResourcePath);
-        // GD.Print(FileAccess.Open(balancePath.ResourcePath, FileAccess.ModeFlags.Read));
-        // balance = new Balance(path);
-        // GD.Print(balance.Balanced(UpgradeType.DIGGER_QUANTITY, 1).Cost);
-        // this.state = new StateCsv(AppDomain.CurrentDomain.BaseDirectory + "/saves");
-    }
-
-    public override void _Process(double delta)
-    {
-        // GD.Print(state.currentLevel(UpgradeType.DIGGER_QUANTITY));
-    }
+	public override void _Process(double delta)
+	{
+		// GD.Print(state.currentLevel(UpgradeType.DIGGER_QUANTITY));
+	}
 }
