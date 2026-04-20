16bc85d44f399c582b39c083124f9eed6b2a95da
Artem Getmanskii <re910@yandex.ru>
2026-04-20 16:37:11 +0300

    add score decremental, add money check to button

--- UI/Button.cs
+++ UI/Button.cs
@@ -5,44 +5,61 @@ using TreasureHunter.save;
 
 public partial class Button : StaticBody2D
 {
-    [Signal]
-    public delegate void UpgradedEventHandler(UpgradeType type, int level, float value, float cost);
+	[Signal]
+	public delegate void UpgradedEventHandler(UpgradeType type, int level, float value, float cost);
 
-    [Export] public UpgradeType type { get; set; }
+	[Export] public UpgradeType type { get; set; }
 
-    public IState state;
-    public IBalance balance;
+	public IState state;
+	public IBalance balance;
+	public Score score;
+	public bool enabled;
 
-    public override void _Ready()
-    {
-        var godot = this.GetStateGd();
-        state = godot.state;
-        balance = godot.balance;
-        InputEvent += MyInputEvent;
-    }
+	public override void _Ready()
+	{
+		var godot = this.GetStateGd();
+		state = godot.state;
+		balance = godot.balance;
+		InputEvent += MyInputEvent;
+		score = this.GetScore();
+		this.enabled = false;
+	}
 
-    public override void _Process(double delta)
-    {
-    }
+	public override void _Process(double delta)
+	{
+		this.enabled = (float)this.balance.Balanced(
+			this.type,
+			this.state.currentLevel(this.type) + 1
+		).Cost <= this.score.score;
+	}
 
-    private void MyInputEvent(Node viewport, InputEvent @event, long shapeIdx)
-    {
-        GD.Print(321);
-        if (@event is InputEventMouseButton mouseEvent)
-        {
-            GD.Print(123);
-            //check money amount
-            var nextLevel = state.currentLevel(type) + 1;
-            if (balance.Checked(type, nextLevel))
-            {
-                var nextValue = balance.Balanced(type, nextLevel);
-                EmitSignalUpgraded(
-                    type,
-                    nextLevel,
-                    (float)nextValue.Value,
-                    (float)nextValue.Cost
-                );
-            }
-        }
-    }
+	private void MyInputEvent(Node viewport, InputEvent @event, long shapeIdx)
+	{
+		if (@event is InputEventMouseButton { ButtonIndex: MouseButton.Left, Pressed: true } && this.enabled == true)
+		{
+			GD.Print(123);
+			// GD.Print("Upgrade cost:");
+			// GD.Print(
+			//     (float)this.balance.Balanced(
+			//         this.type,
+			//         this.state.currentLevel(this.type)
+			//     ).Cost
+			// );
+			// GD.Print("")
+			var nextLevel = state.currentLevel(type) + 1;
+			if (balance.Checked(type, nextLevel))
+			{
+				GD.Print(nextLevel);
+				var nextValue = balance.Balanced(type, nextLevel);
+				this.state.persist(this.type, nextLevel);
+				EmitSignalUpgraded(
+					type,
+					nextLevel,
+					(float)nextValue.Value,
+					(float)nextValue.Cost
+				);
+				this.score.AddScore(-1 * (float)nextValue.Cost);
+			}
+		}
+	}
 }
--- UI/Score.cs
+++ UI/Score.cs
@@ -3,17 +3,17 @@ using System;
 
 public partial class Score : Label
 {
-    private float _score = 0;
-    private float _total = 0;
+    public float score = 0;
+    public float Total = 0;
 
     public void AddScore(float value)
     {
-        _score += value;
+        score += value;
         if (value > 0)
         {
-            _total += value;
+            Total += value;
         }
 
-        Text = "Score: " + _score.ToString("N0");
+        Text = "Score: " + score.ToString("N0");
     }
 }
