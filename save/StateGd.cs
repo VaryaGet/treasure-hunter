ca7571c09f7c0b2fc43e0d8c2cf963a4f46c8a1a
Varya <kotovnav@mail.ru>
2026-04-20 16:11:02 +0300

    add signal

--- save/StateGd.cs
+++ save/StateGd.cs
@@ -7,19 +7,23 @@ using FileAccess = Godot.FileAccess;
 
 public partial class StateGd : Node2D
 {
-	public IState state;
-	public IBalance balance;
+    public IState state;
+    public IBalance balance;
 
-	public override void _Ready()
-	{
-		var text = FileAccess.GetFileAsString("res://resources/LD59.csv");
-		File.WriteAllText(AppDomain.CurrentDomain.BaseDirectory + "/balance.csv", text);
-		this.balance = new Balance(AppDomain.CurrentDomain.BaseDirectory + "/balance.csv");
-		this.state = new StateCsv(AppDomain.CurrentDomain.BaseDirectory + "/saves");
-	}
+    [Signal]
+    public delegate void IsReadyEventHandler();
 
-	public override void _Process(double delta)
-	{
-		// GD.Print(state.currentLevel(UpgradeType.DIGGER_QUANTITY));
-	}
+    public override void _Ready()
+    {
+        var text = FileAccess.GetFileAsString("res://resources/LD59.csv");
+        File.WriteAllText(AppDomain.CurrentDomain.BaseDirectory + "/balance.csv", text);
+        balance = new Balance(AppDomain.CurrentDomain.BaseDirectory + "/balance.csv");
+        state = new StateCsv(AppDomain.CurrentDomain.BaseDirectory + "/saves");
+        EmitSignalIsReady();
+    }
+
+    public override void _Process(double delta)
+    {
+        // GD.Print(state.currentLevel(UpgradeType.DIGGER_QUANTITY));
+    }
 }
