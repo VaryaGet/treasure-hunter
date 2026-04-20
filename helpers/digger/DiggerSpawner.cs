50c3aed59ec10971d290f320e3ec0a78b2a94da5
Varya <kotovnav@mail.ru>
2026-04-20 18:19:48 +0300

    fix

--- helpers/digger/DiggerSpawner.cs
+++ helpers/digger/DiggerSpawner.cs
@@ -14,14 +14,7 @@ public partial class DiggerSpawner : Node2D
     public override void _Ready()
     {
         _playArea = this.GetPlayArea();
-        this.GetStateGd().Ready += OnReady;
-        SetProcess(false);
-    }
-
-    private void OnReady()
-    {
         _diggerHolder = new DiggerHolder(this.GetStateGd());
-        SetProcess(true);
         var btns = GetTree().GetNodesInGroup(Groups.UpgradesBtns).OfType<Btn>();
         foreach (var btn in btns)
         {
